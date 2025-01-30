c4051046854e2819b5b80e25300731d9dfcc2416
abe_nozomu <abe.nozomu@alhinc.jp>
2025-01-30 10:11:40 +0900

    コミット

--- src/main/resources/static/js/history.js
+++ src/main/resources/static/js/history.js
@@ -1,7 +1,9 @@
 const weeks = ['日', '月', '火', '水', '木', '金', '土'];
-let date = new Date(stringDate);
-let year = date.getFullYear();  // 受け取った開始日から年を算出
-let month = date.getMonth() + 1;  // 受け取った開始日から月を算出
+
+//表示する日付・月・年を取得
+let date = new Date(stringDate);　//コントローラーから表示する月の初日を受け取る
+let year = date.getFullYear();  //受け取った開始日から年を算出
+let month = date.getMonth() + 1;  //受け取った開始日から月を算出
 
 function showCalendar(year, month) {
     const calendarHtml = createCalendar(year, month);
@@ -11,24 +13,25 @@ function showCalendar(year, month) {
 }
 
 function createCalendar(year, month) {
-    const startDate = new Date(year, month - 1, 1);  // 月の最初の日を取得
-    const endDate = new Date(year, month, 0);  // 月の最後の日を取得
-    const endDayCount = endDate.getDate();  // 月の末日
-    const startDay = startDate.getDay();  // 月の最初の日の曜日を取得
+    //カレンダー表示に使用する要素を設定
+    const startDate = new Date(year, month - 1, 1);  //表示する月の初日を取得
+    const endDate = new Date(year, month, 0);  //表示する月の最終日を取得
+    const endDayCount = endDate.getDate();  //表示する月の最終日の日にち部分だけを取得
+    const startDay = startDate.getDay();  //表示する月の初日の曜日を取得
 
+    const startDateString = startDate.toISOString().split("T")[0];　//月の初日を"yyyy-MM-dd"形式に変更
+    const endDateString = endDate.toISOString().split("T")[0];　//月の最終日を"yyyy-MM-dd" 形式に変更
+    document.getElementById("previousMonth").value = startDateString;　//前月ボタンのvalue属性に月の初日をセット
+    document.getElementById("nextMonth").value = endDateString;  //次月ボタンのvalue属性に月の最終日をセット
 
-    const startDateString = startDate.toISOString().split("T")[0]; // "yyyy-MM-dd" 形式に変更
-    const endDateString = endDate.toISOString().split("T")[0]; // "yyyy-MM-dd" 形式に変更
-    document.getElementById("previousMonth").value = startDateString;  //前月ボタンのvalueに月の初日をセット
-    document.getElementById("nextMonth").value = endDateString;  //次月ボタンのvalueに月の最終日をセット
+    let dayCount = 1;  //カレンダー表示に使う日にちのカウントの初期値を１に設定
+    let calendarHtml = '';  //HTMLを組み立てる変数。ここにhtmlのコードを追加していく
 
-    let dayCount = 1;  // 日にちのカウント
-    let calendarHtml = '';  // HTMLを組み立てる変数
+    calendarHtml += `<h1>${year}/${month}</h1>`;　//トップに表示する年/月を設定（205/1）
+    calendarHtml += '<table>';　//<table>タグを追加
 
-    calendarHtml += `<h1>${year}/${month}</h1>`;
-    calendarHtml += '<table>';
-
-    // 曜日の行を作成
+    //曜日の行を作成
+    //曜日分の繰り返し処理を行い、曜日を表示する行を追加
     for (let i = 0; i < weeks.length; i++) {
         calendarHtml += `<td class="dateLine">${weeks[i]}</td>`;
     }
